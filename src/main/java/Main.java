5f46f7c60b1e4dc9cddfad05050de51aac8411a1
Amr k <63168118+amrk000@users.noreply.github.com>
2021-07-23 01:56:56 +0200

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -8,20 +8,9 @@ import static spark.Spark.*;
 public class Main {
     public static void main(String[] args){
         
-        port(process.env.PORT);
+        port(80);
         get("/", (req,res)->{
 
-//            Process p = Runtime.getRuntime().exec("traceroute "+req.params(":ip"));
-//            BufferedReader reader = new BufferedReader(new InputStreamReader(p.getInputStream()));
-//            p.waitFor();
-//
-//            if (p.exitValue() == 1) return 1;
-//
-//            StringBuilder Result = new StringBuilder();
-//            while (reader.readLine() != null) Result.append(reader.readLine()).append("</br>");
-//            reader.close();
-//
-//            return Result;
             return "hello world";
         });
 
