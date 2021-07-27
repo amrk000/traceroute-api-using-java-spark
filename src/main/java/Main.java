cd06a1adf8c08645614fb716172a95a68b7d328d
Amr k <63168118+amrk000@users.noreply.github.com>
2021-07-28 00:07:17 +0200

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,8 +6,6 @@ import static spark.Spark.*;
 public class Main {
     public static void main(String[] args) throws InterruptedException {
 
-        port(getHerokuAssignedPort());
-
         get("/traceroute/:ip", (req,res)->{
 
             Process p = Runtime.getRuntime().exec("traceroute -w 2 "+req.params(":ip"));
@@ -24,14 +22,7 @@ public class Main {
         
         get("/", (req,res)->{
 
-            return "Ping Toolkit - Traceroute";
+            return "Linux Traceroute api homepage";
         });
-    }
-    static int getHerokuAssignedPort() {
-        ProcessBuilder processBuilder = new ProcessBuilder();
-        if (processBuilder.environment().get("PORT") != null) {
-            return Integer.parseInt(processBuilder.environment().get("PORT"));
-        }
-        return 4567; //return default port if heroku-port isn't set (i.e. on localhost)
     }
 }
